eab9853a5826129dbcc17820394845025515a24d
Anastasiya-Vavilova <ntttru@bk.ru>
2024-06-04 17:01:57 +0300

    Update BmiService

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,14 +1,14 @@
 public class BmiService {
 
-    public double calculate(int kg, double rost) {
+    public double calculate(int humanWeight, double humanHeight) {
 
         // Формула ИМТ - Например, масса человека = 74 кг, рост = 172 см. Следовательно, индекс массы тела в этом случае равен:
         // ИМТ = 74 : (1,722 в квадрате) ≈ 25,01 кг/м²
-        // heightSquared - Высота в квадрате переводится.
+        // heightSquared - Высота в квадрате.
 
         double bmi;
-        double heightSquared = Math.pow(rost, 2);
-        bmi = kg / heightSquared;
+        double heightSquared = Math.pow(humanHeight, 2);
+        bmi = humanWeight / heightSquared;
 
         return bmi;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
+    // Human weight - вес человека.
+    // Human height - рост человека
     public static void main(String[] args) {
 
-
         BmiService service = new BmiService();
-        int kg = 79;
-        double rost = 1.65;
-        int bmi = (int) service.calculate(kg, rost);
+        int humanWeight = 79;
+        double humanHeight = 1.65;
+        int bmi = (int) service.calculate(humanWeight, humanHeight);
         System.out.println("Индекс массы вашего тела равен " + bmi);
 
 
